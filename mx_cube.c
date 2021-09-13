1ba78d90d93d32bfc27065941d1b4fcdafda3644
Gregory Prokhorov <gprokhorov@freshcode.org>
2021-09-13 20:07:22 +0300

    init

---
+++ mx_cube.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void mx_cube(int n)
+{
+    for (int i = 0; i < (n + n / 2 + 3); i++)
+    {
+        for (int j = 0; j < n / 2 + 2 + 2 + n * 2)
+        {
+        }
+    }
+}
+
+void main()
+{
+    mx_cube(2);
+}
